7a5afe451cef911b8a1078dc6f111d91e1c9bbfa
jslee <jslimited21@gmail.com>
2016-08-20 16:31:59 +0900

    My Django tutorial app, 첫번째 커밋

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# def detail(request, question_id):
+#   return HttpResponse("You're looking at question %s."%question_id)
+
+
+# def results(request, question_id):
+#   response="You're looking at the results of question %s."
+#   return HttpResponse("response %s."%question_id)
+
+# def vote(request, question_id):
+#   return HttpResponse("You're votiong on question %s."%question_id)
